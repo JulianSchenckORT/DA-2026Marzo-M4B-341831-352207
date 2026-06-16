8665a01dc08dd26b8a2cd7d363133a0cb55befc9
Julian Schenck <julianschenck26@gmail.com>
2026-06-16 14:17:27 -0300

    springboot

---
+++ src/main/java/ort/da/obligatoriodiseno/ObligatoriodisenoApplication.java
@@ -0,0 +1,13 @@
+package ort.da.obligatoriodiseno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObligatoriodisenoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObligatoriodisenoApplication.class, args);
+	}
+
+}
